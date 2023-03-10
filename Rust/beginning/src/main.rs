c0eaf2d605f4e427fbf4de74ecc4215bb21a3dcc
samurai <963513255@qq.com>
2023-03-10 15:08:03 +0800

    &

--- Rust/beginning/src/main.rs
+++ Rust/beginning/src/main.rs
@@ -1,24 +1,9 @@
-use std::io::*;
-
-fn fun(n: &i32) -> i32 {
-    if n % 2 == 0 {
-        n / 2
-    } else {
-        (3 * n + 1) / 2
-    }
+fn main() {
+    let mut s = String::from("value");
+    fun(&mut s);
+    println!("{}", s);
 }
 
-fn main() {
-    let mut s = String::new();
-    stdin().read_line(&mut s).unwrap();
-    let mut n = 0;
-    for i in s.trim().split(' ') {
-        n = i.parse().unwrap();
-    }
-    let mut c = 0;
-    while n != 1 {
-        n = fun(&n);
-        c += 1;
-    }
-    println!("{}", c);
+fn fun(s: &mut String) {
+    s.push_str("string");
 }
