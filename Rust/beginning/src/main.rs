3ad2fea7cf4f073bbae3da055ad1ea1ab1e80dd8
samurai <963513255@qq.com>
2023-03-10 09:08:34 +0800

    main

--- Rust/beginning/src/main.rs
+++ Rust/beginning/src/main.rs
@@ -1,7 +1,7 @@
 fn main() {
     let i = Some(14);
     let s = Some("String");
-    let c = Some('c');
+    let c = Some('d');
     let a: Option<i32> = None;
     let x = 5;
     //不能用i32与Option<i32>相加
