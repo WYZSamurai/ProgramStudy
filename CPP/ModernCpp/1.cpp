fe6805b66f521f359ff6da79c152eb7726c16458
samurai <963513255@qq.com>
2023-03-15 16:46:39 +0800

    test

--- CPP/ModernCpp/1.cpp
+++ CPP/ModernCpp/1.cpp
@@ -4,6 +4,17 @@
 using namespace std;
 
 int main() {
-  auto i = 0;
+  auto n = 0;
   vector<int> v;
+  cin >> n;
+  for (auto i = 0; i < n; i++) {
+    int x;
+    cin >> x;
+    v.push_back(x);
+  }
+  sort(v.begin(), v.end());
+  for (auto &i : v) {
+    cout << i << ' ';
+  }
+  cout << endl;
 }
