4312b6a13ca3e1d04c398ad02273e3a27418313a
samurai <963513255@qq.com>
2023-03-17 10:32:01 +0800

    upload

--- Rust/beginning/src/main.rs
+++ Rust/beginning/src/main.rs
@@ -1,25 +1,31 @@
 use std::io::*;
 
-fn fio() -> (Vec<String>, usize) {
+fn fio() -> (Vec<i32>, i32) {
     let mut n = String::new();
     stdin().read_line(&mut n).unwrap();
-    let mut n: usize = n.trim().parse().unwrap();
+    let mut n: i32 = n.trim().parse().unwrap();
     let n1 = n;
-    let mut v: Vec<String> = Vec::new();
+    let mut v: Vec<i32> = Vec::new();
     while n > 0 {
         let mut v1 = String::new();
         stdin().read_line(&mut v1).unwrap();
-        v.push(v1.trim().to_string());
+        v.push(v1.trim().parse().unwrap());
         n -= 1;
     }
     (v, n1)
 }
 
 fn main() {
-    fun();
+    let (mut v, n) = fio();
+    let mut n = String::new();
+    stdin().read_line(&mut n).unwrap();
+    let n: i32 = n.trim().parse().unwrap();
+    fun(&mut v, n);
 }
 
-fn fun() {
-    let (v, n) = fio();
-    for _i in 0..n {}
+fn fun(v: &mut Vec<i32>, n: i32) -> (i32, i32) {
+    for &mut i in v {
+        let tar = n - i;
+    }
+    (1, 2)
 }
