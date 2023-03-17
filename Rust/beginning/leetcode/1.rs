1ef12f159ca9d4fe72f03a97163c614194533a92
Samurai <963513255@qq.com>
2023-03-17 18:15:50 +0800

    test

--- Rust/beginning/leetcode/1.rs
+++ Rust/beginning/leetcode/1.rs
@@ -1,1 +1,29 @@
+use std::collections::HashMap;
+use std::io::*;
+enum Solution {}
+impl Solution {
+    pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        let mut map: HashMap<i32, usize> = HashMap::new();
+        for i in 0..nums.len() {
+            if let Some(y) = map.get(&(target - nums[i])) {
+                return vec![*y as i32, i as i32];
+            }
+            map.insert(nums[i], i);
+        }
+        panic!("not found!")
+    }
+}
 
+fn main() {
+    let mut n = String::new();
+    stdin().read_line(&mut n).unwrap();
+    let mut v: Vec<i32> = Vec::new();
+    for i in n.trim().split(" ") {
+        v.push(i.parse().unwrap());
+    }
+    n.clear();
+    stdin().read_line(&mut n).unwrap();
+    let n: i32 = n.trim().parse().unwrap();
+    let s = Solution::two_sum(v, n);
+    println!("{:?}", s);
+}
--- Rust/beginning/src/main.rs
+++ Rust/beginning/src/main.rs
@@ -1,31 +1,3 @@
-use std::io::*;
-
-fn fio() -> (Vec<i32>, i32) {
-    let mut n = String::new();
-    stdin().read_line(&mut n).unwrap();
-    let mut n: i32 = n.trim().parse().unwrap();
-    let n1 = n;
-    let mut v: Vec<i32> = Vec::new();
-    while n > 0 {
-        let mut v1 = String::new();
-        stdin().read_line(&mut v1).unwrap();
-        v.push(v1.trim().parse().unwrap());
-        n -= 1;
-    }
-    (v, n1)
-}
-
 fn main() {
-    let (mut v, n) = fio();
-    let mut n = String::new();
-    stdin().read_line(&mut n).unwrap();
-    let n: i32 = n.trim().parse().unwrap();
-    fun(&mut v, n);
-}
-
-fn fun(v: &mut Vec<i32>, n: i32) -> (i32, i32) {
-    for &mut i in v {
-        let tar = n - i;
-    }
-    (1, 2)
+    panic!("dde")
 }
