d99157aebc84903ae5551508676ea70f8e48f9f6
Samurai <963513255@qq.com>
2023-03-18 17:18:31 +0800

    test

--- CPP/codeforces/1B.cpp
+++ CPP/codeforces/1B.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
+#include <string>
 using namespace std;
+
+struct s {
+  int a;
+  string str;
+};
 
 int main() {
   auto a = 10;
   cout << a << endl;
+  s s1 = {12, "deded"};
+  cout << s1.a << ' ' << s1.str << endl;
 }
