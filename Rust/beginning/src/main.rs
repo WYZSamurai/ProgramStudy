b95d17c42d10f23163d7913a25ec8c540bdd6fbf
Samurai <963513255@qq.com>
2023-03-22 18:20:51 +0800

    fix

--- Rust/beginning/src/main.rs
+++ Rust/beginning/src/main.rs
@@ -1,27 +1,3 @@
-struct Man {
-    name: String,
-    age: i8,
-}
-impl Man {
-    fn new(name: String, age: i8) -> Self {
-        Self { name, age }
-    }
-}
-
-enum Item {
-    Apple { kg: i8, price: i8 },
-    Peach(i32),
-    Watermelon,
-}
-
 fn main() {
-    let d = String::from("Yizhen");
-    let m1 = Man::new(d, 12);
-    println!("{} {}", m1.name, m1.age);
-    let i1 = Item::Apple {
-        kg: (12),
-        price: (30),
-    };
-    let i2 = Item::Peach(2);
-    let i3 = Item::Watermelon;
+    println!("world");
 }
