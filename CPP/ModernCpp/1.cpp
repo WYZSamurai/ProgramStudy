08290240aa6cb6fd01ad9c6acf2c76c05e55514a
Samurai <963513255@qq.com>
2023-03-29 20:37:09 +0800

    fix

--- CPP/ModernCpp/1.cpp
+++ CPP/ModernCpp/1.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
-using namespace std;
-
 int main()
 {
-  cout << "HelloWorld" << endl;
+  std::cout << "打印 ASCII表:\n";
+  for (char i = 32; i < 127; ++i)
+  {
+    std::cout << i << ' ';
+    if (i % 16 == 15)
+      std::cout << '\n';
+  }
+  std::cout << std::endl;
   system("pause");
 }
