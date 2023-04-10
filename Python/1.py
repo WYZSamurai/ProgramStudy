cfbba9492aa9df2d13fc014e1cb866d80ad39bb2
samurai <963513255@qq.com>
2023-04-10 15:24:54 +0800

    完成4

--- Python/1.py
+++ Python/1.py
@@ -1,1 +1,9 @@
-print("")
+if __name__ == "__main__":
+    a = "swsw"
+    print(a)
+
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        v = list
+        map = dict
