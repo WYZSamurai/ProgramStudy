48226c3df1c02d2d2f26a6caf7bfd491f994e0ab
Samurai <963513255@qq.com>
2023-04-10 20:19:58 +0800

    lf

--- Rust/beginning/src/main.rs
+++ Rust/beginning/src/main.rs
@@ -1,12 +1,13 @@
+use std::collections::HashMap;
 use std::io::stdin;
 
 fn main() {
     let (vec, target) = iom();
-    let s = Solution::find_median_sorted_arrays(vec, target);
+    let s = Solution::two_sum(vec, target);
     println!("{:?}", s);
 }
 
-fn iom() -> (Vec<i32>, Vec<i32>) {
+fn iom() -> (Vec<i32>, i32) {
     let mut v: Vec<i32> = Vec::new();
     let mut s = String::new();
     stdin().read_line(&mut s).unwrap();
@@ -14,26 +15,24 @@ fn iom() -> (Vec<i32>, Vec<i32>) {
         v.push(i.to_string().parse().unwrap());
     }
     s.clear();
-    let mut v2: Vec<i32> = Vec::new();
     stdin().read_line(&mut s).unwrap();
-    for i in s.trim().split(" ") {
-        v2.push(i.to_string().parse().unwrap());
-    }
-    (v, v2)
+    let target: i32 = s.trim().parse().unwrap();
+    (v, target)
 }
 struct Solution {}
 impl Solution {
-    pub fn find_median_sorted_arrays(nums1: Vec<i32>, nums2: Vec<i32>) -> f64 {
-        let mut v1 = nums1;
-        let mut v2 = nums2;
-        v1.append(&mut v2);
-        v1.sort_unstable_by(|x, y| x.cmp(y));
-        let len = v1.len();
-        let res: f64;
-        if len % 2 == 0 {
-            res = (v1[len / 2 - 1] + v1[len / 2]) as f64 / 2 as f64;
-        } else {
-            res = v1[len / 2] as f64;
+    pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        let mut res: Vec<i32> = Vec::new();
+        let mut map: HashMap<i32, i32> = HashMap::new();
+        for i in 0..nums.len() {
+            // 求出nums[i]对应的x值，如果map中有对应的x值，则输出角标
+            // 否则向map输入i及其对应的值
+            if map.contains_key(&(target - nums[i])) {
+                res.push(*map.get(&(target - nums[i])).unwrap());
+                res.push(i as i32);
+            } else {
+                map.insert(nums[i], i as i32);
+            }
         }
         res
     }
