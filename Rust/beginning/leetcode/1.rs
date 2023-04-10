3f6f6a2523331251b34244d80bde7bc0ea20c932
samurai <963513255@qq.com>
2023-04-10 11:31:56 +0800

    leetcode 1

--- Rust/beginning/leetcode/1.rs
+++ Rust/beginning/leetcode/1.rs
@@ -1,29 +1,39 @@
 use std::collections::HashMap;
-use std::io::*;
-enum Solution {}
-impl Solution {
-    pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
-        let mut map: HashMap<i32, usize> = HashMap::new();
-        for i in 0..nums.len() {
-            if let Some(y) = map.get(&(target - nums[i])) {
-                return vec![*y as i32, i as i32];
-            }
-            map.insert(nums[i], i);
-        }
-        panic!("not found!")
-    }
-}
+use std::io::stdin;
 
 fn main() {
-    let mut n = String::new();
-    stdin().read_line(&mut n).unwrap();
-    let mut v: Vec<i32> = Vec::new();
-    for i in n.trim().split(" ") {
-        v.push(i.parse().unwrap());
-    }
-    n.clear();
-    stdin().read_line(&mut n).unwrap();
-    let n: i32 = n.trim().parse().unwrap();
-    let s = Solution::two_sum(v, n);
+    let (vec, target) = iom();
+    let s = Solution::two_sum(vec, target);
     println!("{:?}", s);
+}
+
+fn iom() -> (Vec<i32>, i32) {
+    let mut v: Vec<i32> = Vec::new();
+    let mut s = String::new();
+    stdin().read_line(&mut s).unwrap();
+    for i in s.trim().split(" ") {
+        v.push(i.to_string().parse().unwrap());
+    }
+    s.clear();
+    stdin().read_line(&mut s).unwrap();
+    let target: i32 = s.trim().parse().unwrap();
+    (v, target)
+}
+struct Solution {}
+impl Solution {
+    pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        let mut res: Vec<i32> = Vec::new();
+        let mut map: HashMap<i32, i32> = HashMap::new();
+        for i in 0..nums.len() {
+            // 求出nums[i]对应的x值，如果map中有对应的x值，则输出角标
+            // 否则向map输入i及其对应的值
+            if map.contains_key(&(target - nums[i])) {
+                res.push(*map.get(&(target - nums[i])).unwrap());
+                res.push(i as i32);
+            } else {
+                map.insert(nums[i], i as i32);
+            }
+        }
+        res
+    }
 }
--- Rust/beginning/src/main.rs
+++ Rust/beginning/src/main.rs
@@ -1,48 +1,39 @@
+use std::collections::HashMap;
 use std::io::stdin;
 
 fn main() {
+    let (vec, target) = iom();
+    let s = Solution::two_sum(vec, target);
+    println!("{:?}", s);
+}
+
+fn iom() -> (Vec<i32>, i32) {
+    let mut v: Vec<i32> = Vec::new();
     let mut s = String::new();
     stdin().read_line(&mut s).unwrap();
-    let mut v: Vec<i32> = Vec::new();
     for i in s.trim().split(" ") {
         v.push(i.to_string().parse().unwrap());
     }
-    let (n, m) = (*v.get(0).unwrap(), *v.get(1).unwrap());
-    fun(n, m);
+    s.clear();
+    stdin().read_line(&mut s).unwrap();
+    let target: i32 = s.trim().parse().unwrap();
+    (v, target)
 }
-
-fn fun(n: i32, m: i32) {
-    let (mut c1, mut c2) = (0, 0);
-    let mut x = 1;
-    while c1 < m {
-        if is_prime(&x) {
-            c1 += 1;
-            if c1 >= n {
-                c2 += 1;
-                if c1 == m {
-                    print!("{}", x);
-                } else if c2 % 10 != 0 {
-                    print!("{} ", x);
-                } else {
-                    println!("{}", x);
-                }
+struct Solution {}
+impl Solution {
+    pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        let mut res: Vec<i32> = Vec::new();
+        let mut map: HashMap<i32, i32> = HashMap::new();
+        for i in 0..nums.len() {
+            // 求出nums[i]对应的x值，如果map中有对应的x值，则输出角标
+            // 否则向map输入i及其对应的值
+            if map.contains_key(&(target - nums[i])) {
+                res.push(*map.get(&(target - nums[i])).unwrap());
+                res.push(i as i32);
+            } else {
+                map.insert(nums[i], i as i32);
             }
         }
-        x += 1;
+        res
     }
-}
-
-fn is_prime(n: &i32) -> bool {
-    if *n <= 1 {
-        return false;
-    }
-    if *n % 2 == 0 && *n != 2 {
-        return false;
-    }
-    for i in (3..=(*n as f32).sqrt() as i32).step_by(2) {
-        if *n % i == 0 {
-            return false;
-        }
-    }
-    true
 }
