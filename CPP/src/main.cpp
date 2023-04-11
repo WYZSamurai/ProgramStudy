28e4daa82abbe97aa98ac430f2eb3fa0e26423e5
Samurai <963513255@qq.com>
2023-04-11 19:16:15 +0800

    c++ io

--- CPP/src/main.cpp
+++ CPP/src/main.cpp
@@ -1,6 +1,17 @@
 #include<iostream>
+#include<algorithm>
+#include<vector>
+#include<string>
 using namespace std;
 
+void iom() {
+    string s;
+    // 读入行
+    getline(cin, s);
+    auto a = 0;
+    cout << s << endl;
+}
+
 int main() {
-    cout << "HelloWorld" << endl;
+    iom();
 }
---
+++ Doc/C++/io.md
@@ -0,0 +1,6 @@
+1. char转int：char-'0'
+2. int转string：to_string(int)
+3. string转int：stoi(string)
+****
+1. 获取vector中第n个元素：vector.at(n-1)
+2. 插入vector第n个位置前：vector.insert(vector.begin()+n-1,val)
