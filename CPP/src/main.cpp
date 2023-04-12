ccf306a3908de7e8cb01e6dff8941da183913445
Samurai <963513255@qq.com>
2023-04-12 15:47:23 +0800

    完成天线第一章

--- CPP/src/main.cpp
+++ CPP/src/main.cpp
@@ -9,13 +9,5 @@ int main() {
     vector<int> v;
     // C++20 新的通用初始化
     int x{ 1 };
-    while (getchar() == '\n') {
-        int i;
-        cin >> i;
-        v.push_back(i);
-    }
-    for (auto& i : v) {
-        cout << i;
-    }
     cout << x << endl;
 }
--- Rust/beginning/src/main.rs
+++ Rust/beginning/src/main.rs
@@ -1,3 +1,24 @@
+use std::io::stdin;
 fn main() {
-    println!("huh");
+    let v = iom();
+    let s = Solution::max_area(v);
+    println!("{}", s);
+}
+
+struct Solution;
+impl Solution {
+    pub fn max_area(height: Vec<i32>) -> i32 {
+        let mut result = 1;
+        result
+    }
+}
+
+fn iom() -> Vec<i32> {
+    let mut s = String::new();
+    let mut v: Vec<i32> = Vec::new();
+    stdin().read_line(&mut s).unwrap();
+    for i in s.trim().split(" ") {
+        v.push(i.to_string().parse().unwrap());
+    }
+    v
 }
