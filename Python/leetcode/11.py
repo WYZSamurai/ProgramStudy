6e0f3ba88b17ae7f801541668b3b87379ba6b5aa
Samurai <963513255@qq.com>
2023-04-20 18:34:26 +0800

    完成11

--- Python/leetcode/11.py
+++ Python/leetcode/11.py
@@ -1,27 +1,23 @@
 class Solution:
-    # 容积为较低的一边为高，两边索引之差为低的面积
-    def vol(self, i1, i2, h1, h2):
-        h = 0
-        if h1 < h2:
-            h = h1
-        else:
-            h = h2
-        d = abs(i1 - i2)
-        return h * d
-
     def maxArea(self, height: list[int]) -> int:
         res: int = 0
         length = len(height)
-        (i1, i2) = (0, 0)
-        for i1 in range(0, length):
-            for i2 in range(i1 + 1, length):
-                tmp = Solution().vol(i1=i1, i2=i2, h1=height[i1], h2=height[i2])
-                if res < tmp:
-                    res = tmp
-
+        (start, end) = (0, length - 1)
+        while start < end:
+            d = end - start
+            h = 0
+            if height[start] < height[end]:
+                h = height[start]
+                start += 1
+            else:
+                h = height[end]
+                end -= 1
+            tmp = d * h
+            if res < tmp:
+                res = tmp
         return res
 
 
 if __name__ == "__main__":
     num1 = [1, 8, 6, 2, 5, 4, 8, 3, 7]
-    print("结果为：", Solution.maxArea(Solution, height=num1))
+    print("结果为：", Solution.maxArea(Solution, height=num1), end="")
--- Rust/beginning/src/main.rs
+++ Rust/beginning/src/main.rs
@@ -1,26 +1,21 @@
 struct Solution {}
 impl Solution {
-    // 始终保存当前最大的面积，容积高度为较低侧的高度，宽为两个指针的举例
-    fn vol(i: usize, j: usize, h1: i32, h2: i32) -> i32 {
-        let h: i32;
-        let d = (j - i) as i32;
-        if h1 < h2 {
-            h = h1;
-        } else {
-            h = h2;
-        }
-        return h * d;
-    }
-
     pub fn max_area(height: Vec<i32>) -> i32 {
         let mut res = 0;
-        let len = height.len();
-        for i in 0..len {
-            for j in i + 1..len {
-                let tmp = Solution::vol(i, j, height[i], height[j]);
-                if res < tmp {
-                    res = tmp;
-                }
+        let (mut start, mut end) = (0, height.len() - 1);
+        while start < end {
+            let d = end - start;
+            let h: i32;
+            if height[start] < height[end] {
+                h = height[start];
+                start += 1;
+            } else {
+                h = height[end];
+                end -= 1;
+            }
+            let tmp = d as i32 * h;
+            if res < tmp {
+                res = tmp;
             }
         }
         return res;
@@ -28,6 +23,6 @@ impl Solution {
 }
 
 fn main() {
-    let nums1 = vec![1, 8, 6, 2, 5, 4, 8, 3, 7];
-    println!("{}", Solution::max_area(nums1));
+    let nums1 = vec![-1, 0, 1, 2, -1, -4];
+    println!("结果为：{}", Solution::max_area(nums1));
 }
