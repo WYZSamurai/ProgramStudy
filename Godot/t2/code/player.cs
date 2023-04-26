8fb010baf38cbf3d92bececa9305d0c65eacfa96
Samurai <963513255@qq.com>
2023-04-26 16:36:23 +0800

    完成有效口径

--- Godot/t2/code/player.cs
+++ Godot/t2/code/player.cs
@@ -1,12 +1,12 @@
 using Godot;
 using System;
 
-public partial class main : Node
+public partial class player : Sprite2D
 {
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
-        GD.Print("main准备就绪");
+        Console.WriteLine("player准备就绪");
     }
 
     // Called every frame. 'delta' is the elapsed time since the previous frame.
