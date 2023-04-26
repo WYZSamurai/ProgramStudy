2b1f635fbfe525add28bc849b011fd12bdfbe1d9
Samurai <963513255@qq.com>
2023-04-26 21:50:49 +0800

    开始15

---
+++ Python/leetcode/15.py
@@ -0,0 +1,11 @@
+class Solution:
+    def threeSum(self, nums: list[int]) -> list[list[int]]:
+        num1 = [1]
+        num2 = [2]
+        res = [num1, num2]
+        return res
+
+
+if __name__ == "__main__":
+    nums = [-1, 0, 1, 2, -1, -4]
+    print("结果为：", Solution.threeSum(Solution, nums=nums))
--- Rust/beginning/leetcode/11.rs
+++ Rust/beginning/leetcode/11.rs
@@ -1,24 +1,23 @@
-use std::io::stdin;
-fn main() {
-    let v = iom();
-    let s = Solution::max_area(v);
-    println!("{}", s);
-}
-
-struct Solution;
+struct Solution {}
 impl Solution {
     pub fn max_area(height: Vec<i32>) -> i32 {
-        let mut result = 1;
-        result
+        let mut res = 0;
+        let (mut start, mut end) = (0, height.len() - 1);
+        while start < end {
+            let d = end - start;
+            let h: i32;
+            if height[start] < height[end] {
+                h = height[start];
+                start += 1;
+            } else {
+                h = height[end];
+                end -= 1;
+            }
+            let tmp = d as i32 * h;
+            if res < tmp {
+                res = tmp;
+            }
+        }
+        return res;
     }
-}
-
-fn iom() -> Vec<i32> {
-    let mut s = String::new();
-    let mut v: Vec<i32> = Vec::new();
-    stdin().read_line(&mut s).unwrap();
-    for i in s.trim().split(" ") {
-        v.push(i.to_string().parse().unwrap());
-    }
-    v
 }
