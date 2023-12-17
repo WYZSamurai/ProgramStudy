7c8a742c60c85d12f38794dca9b9d2c508a7f526
Samurai <963513255@qq.com>
2023-12-17 13:06:21 +0800

    xsxs

--- Rust/beginning/src/main.rs
+++ Rust/beginning/src/main.rs
@@ -1,18 +1,3 @@
-use std::io::stdin;
-
-fn func(f: f64, n: f64, h: f64) -> f64 {
-    let w = (150.0 / f) * ((n + 1.0) / 2.0).powf(-0.5);
-    println!("{}", h);
-    return w;
-}
-
 fn main() {
-    let mut v: Vec<f64> = Vec::new();
-    let mut s = String::new();
-    stdin().read_line(&mut s).unwrap();
-    for i in s.trim().split(" ") {
-        v.push(i.to_string().parse().unwrap())
-    }
-    let (f, n, h) = (v[0], v[1], v[2]);
-    println!("{}", func(f, n, h));
+    println!("xssxsx");
 }
