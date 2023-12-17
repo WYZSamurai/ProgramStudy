63efe489a240b8cef34d082e0415e3b9dba674fd
Samurai <963513255@qq.com>
2023-12-17 14:37:58 +0800

    xsxs

--- C#/main.cs
+++
@@ -1,5 +0,0 @@
-Console.WriteLine("xcscscs");
-var a = 1;
-var name = Console.ReadLine();
-Console.WriteLine("{0}\n{1}", name, a);
-Console.ReadLine();
---
+++ C#/src/main.cs
@@ -0,0 +1,5 @@
+using MathWorks.MATLAB.NET.Arrays;
+using Mytest;
+Class1 fun = new Class1();
+fun.test();
+Console.ReadLine();
